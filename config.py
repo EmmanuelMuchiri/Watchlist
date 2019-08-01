f6590fcee8a01e3016eaa27fd8696a9577837445
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-01 20:33:48 +0300

    trying deployment

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/watchlist'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     #  email configurations
@@ -17,25 +17,23 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
+    
+    
     @staticmethod
     def init_app(app):
         pass
 
 
 class ProdConfig(Config):
-    pass
-
-
-class DevConfig(Config):
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/watchlist'
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/watchlist'
     DEBUG = True
 
 
